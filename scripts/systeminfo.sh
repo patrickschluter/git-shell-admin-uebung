b93cc934f8036f5eedfe50d5f72025e8363e33b8
Max Mustermann <max@example.com>
2026-08-20 11:18:18 +0200

    systeminfo

---
+++ scripts/systeminfo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "=============================="
+echo "       SYSTEMINFORMATIONEN"
+echo "=============================="
+
+echo "Betriebssystem:"
+uname -s
+
+echo "Hostname:"
+hostname
+
+echo "Kernel:"
+uname -r
+
+echo "Benutzer:"
+whoami
+
+echo "Aktuelles Datum:"
+date
+
+echo "=============================="
